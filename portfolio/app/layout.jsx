bed34e2dd4464b5acbeb73caebdaa6d8324358db
Nick Edwards <nledwa01@gmail.com>
2026-05-29 15:40:47 -0500

    Initial project and plugin set up

---
+++ portfolio/app/layout.jsx
@@ -0,0 +1,23 @@
+import { Nunito_Sans } from 'next/font/google';
+import './globals.css';
+
+const nunitoSans = Nunito_Sans({
+    variable: '--font-nunito-sans',
+    subsets: ['sans-serif'],
+});
+
+export const metadata = {
+    title: 'Nick Edwards - Dev & Design',
+    description:
+        'Welcome to my website! Please take a look at my projects and experience.',
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang='en' className={`${nunitoSans.variable} h-full antialiased`}>
+            <body className='font-nunito-sans flex min-h-full flex-col'>
+                {children}
+            </body>
+        </html>
+    );
+}
